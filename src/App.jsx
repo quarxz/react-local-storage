822c45a50d32136731fe9aa23e966d5e0fed6ab4
Falk König <fk@falkkoenig.de>
2024-02-16 08:42:18 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
+// Speichern von Smart User Information
+// dark mode, status
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(() => {
+    const newCounter = localStorage.getItem("counter");
+    if (newCounter) {
+      return JSON.parse(newCounter);
+    } else {
+      return 0;
+    }
+  });
+  const [user, setUser] = useState(() => {
+    const newUser = localStorage.getItem("user");
+    if (newUser) {
+      return JSON.parse(newUser);
+    } else {
+      return {
+        name: "",
+        email: "",
+      };
+    }
+  });
+  const [inputUser, setInputUser] = useState({
+    name: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    function saveToLocalStorage() {
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("counter", JSON.stringify(counter));
+    }
+    saveToLocalStorage();
+  }, [user]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      {user.name ? (
+        <div className="counter-content">
+          <button
+            onClick={() => {
+              localStorage.setItem("counter", JSON.stringify(counter + 1));
+              setCounter((prev) => prev + 1);
+            }}
+          >
+            +
+          </button>
+          <p className="counter">{counter}</p>
+          <button
+            onClick={() => {
+              localStorage.setItem("counter", JSON.stringify(counter - 1));
+              setCounter((prev) => prev - 1);
+            }}
+          >
+            -
+          </button>
+        </div>
+      ) : undefined}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setUser({ ...inputUser });
+          setInputUser({ name: "", email: "" });
+        }}
+      >
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="text"
+          value={inputUser.name}
+          onChange={(e) => {
+            setInputUser({ ...inputUser, name: e.target.value });
+          }}
+        />
+        <label htmlFor="email">eMail:</label>
+        <input
+          id="email"
+          type="email"
+          value={inputUser.email}
+          onChange={(e) => {
+            setInputUser({ ...inputUser, email: e.target.value });
+          }}
+        />
+        {user.name ? undefined : <button typeof="submit">Login</button>}
+      </form>
+      <h2>Logged User:</h2>
+      <p>name: {user.name}</p>
+      <p>email: {user.email}</p>
+      {user.name ? (
+        <button
+          onClick={() => {
+            setUser({ name: "", email: "" });
+            setCounter(0);
+          }}
+        >
+          Logout
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      ) : undefined}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
